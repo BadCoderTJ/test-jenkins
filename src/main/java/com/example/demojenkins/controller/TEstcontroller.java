619c1cc64ed4d646534fcab0e77ca054512fc1b6
tianjie <tianjiegsd@gmail.com>
2022-09-26 21:25:50 +0800

    first commit

---
+++ src/main/java/com/example/demojenkins/controller/TEstcontroller.java
@@ -0,0 +1,18 @@
+package com.example.demojenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tianjie
+ * @description
+ * @date 22/9/25 0025 19:39
+ */
+@RestController
+public class TEstcontroller {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
